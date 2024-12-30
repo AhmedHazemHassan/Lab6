c8219aeda02a0fb21789185741125799607d24bd
Ahmed Hazem Hassan <168864264+AhmedHazemHassan@users.noreply.github.com>
2024-12-31 00:25:49 +0200

    Update test_Calc_Add.py

--- test_Calc_Add.py
+++ test_Calc_Add.py
@@ -4,6 +4,6 @@ from Calc_Add import Calculator_function
 calc = Calculator_function()
 
 class TestCalc_Add(unittest.TestCase):
-    def test_add(self): self.assertEqual(calc.add(2, 3), str(5))
-    def test_add(self): self.assertEqual(calc.add(-1, 1), str(0))
-    def test_add(self): self.assertEqual(calc.add(100, 200), str(300))
+    def test_add(self): self.assertEqual(calc.add(2, 3), 5)
+    def test_add(self): self.assertEqual(calc.add(-1, 1), 0)
+    def test_add(self): self.assertEqual(calc.add(100, 200), 300)
